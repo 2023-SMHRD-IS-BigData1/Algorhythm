777782ac92ab2b3a908d4086fa08d42c5d6136ff
yundae_ <ysc0203020@naver.com>
2023-12-03 19:22:17 +0900

    fix: add login and join
    로그인 기능과 회원가입 기능 만들었습니다.
    추후에 진척도 확인 후에 비동기 방식으로 전환 하겠습니다.
    로그인 실패 회원가입 실패시 모달창 필요합니다.

--- spring/algo/src/main/java/com/smhrd/algo/controller/WeatherAPIController.java
+++ spring/algo/src/main/java/com/smhrd/algo/controller/WeatherAPIController.java
@@ -1,10 +1,7 @@
 package com.smhrd.algo.controller;
 
 import com.smhrd.algo.model.dto.WeatherResponse;
-import com.smhrd.algo.repository.BikeStationRepository;
-import com.smhrd.algo.service.UserService;
 import com.smhrd.algo.service.WeatherService;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
@@ -13,13 +10,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.net.URISyntaxException;
 
-@RequiredArgsConstructor
 @Log4j2
+@RequiredArgsConstructor
 @Controller
-public class TestController {
-
-    private final UserService userService;
-    private final BikeStationRepository bikeStationRepository;
+public class WeatherAPIController {
     private final WeatherService weatherService;
 
 
@@ -43,21 +37,4 @@ public class TestController {
     public String predictWhether() {
         return weatherService.getSevenDayData();
     }
-
-    @PostConstruct
-    public void init() {
-
-        log.debug(bikeStationRepository.findAll().size());
-
-        userService.createUser("admin","1234","최윤석");
-
-    }
-
-    @GetMapping("/logintest")
-    public String login() {
-        return "contact";
-    }
-
-
-
 }
--- spring/algo/src/main/java/com/smhrd/algo/repository/UserRepository.java
+++ spring/algo/src/main/java/com/smhrd/algo/repository/UserRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUserIdAndUserPw(String userId, String userPw);
 
+    User findByUserId(String userId);
+
 }
