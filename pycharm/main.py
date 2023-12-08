4cc8a776b68022f918801f62b334bf87e12c8483
oolongtea111 <oolongtea1205@gmail.com>
2023-12-08 09:15:31 +0900

    pycharm

--- pycharm/main.py
+++ pycharm/main.py
@@ -5,6 +5,8 @@ from pydantic import BaseModel
 import json
 from datetime import datetime, timedelta
 import pandas as pd
+import numpy as np
+
 """
     port 5000
     uvicorn main:app --reload --host 0.0.0.0 --port 5000
@@ -17,6 +19,7 @@ app = FastAPI()
 # Request 받을 객체 생성
 class Item(BaseModel):
     json: List[str]
+
 
 
 # 포스트 형식으로 정거장 리스트 받아오기
@@ -85,38 +88,47 @@ async def predict(item: Item):
     print(combined_df)
 
 
-    # 강수 확률 데이터 전처리
-    weatherDataCall = treeWeatherData['response']['body']['items']['item'][0]
+    # # 강수 확률 데이터 전처리
+    # weatherDataCall = treeWeatherData['response']['body']['items']['item'][0]
+    #
+    # print(weatherDataCall)
+    # # 3일부터10일까지 강수확률 데이터에서 "regId":"11C20000"인 데이터 추출
+    # weather_data_list = [data1 for data1 in treeWeatherData['response']['body']['items']['item'] if
+    #                      data1.get('regId') == '11C20000']
+    #
+    # print(weather_data_list)
+    #
+    # # 데이터프레임 생성
+    # weather_df = pd.DataFrame(weather_data_list, index=None)
+    #
+    # selected_columns3 = ['rnSt3Am', 'rnSt3Pm', 'rnSt4Am', 'rnSt4Pm', 'rnSt5Am', 'rnSt5Pm', 'rnSt6Am', 'rnSt6Pm',
+    #                      'rnSt7Am', 'rnSt7Pm', 'rnSt8', 'rnSt9', 'rnSt10']
+    #
+    #
+    # # 인덱스 삭제
+    # weather_df_selected3 = weather_df[selected_columns3].reset_index(drop=True)
+    #
+    # weather_df_selected4 = pd.DataFrame(weather_df_selected3)
+    #
+    # # 결과 출력
+    # print(weather_df_selected4)
+    # print(weather_df_selected4.shape)
+    # for i in range(3, 8):
+    #     am_col = f'rnSt{i}Am'
+    #     pm_col = f'rnSt{i}Pm'
+    #     new_col = f'rnSt{i}'
+    #     # 해당 일자에 대한 조건식을 작성하여 새로운 컬럼 추가
+    #     weather_df_selected4[new_col] = weather_df_selected4.where((weather_df_selected4[am_col] >= 50) | (weather_df_selected4[pm_col] >= 50))
+        # weather_df_selected4.drop(am_col, axis=1, inplace=True)
+        # weather_df_selected4.drop(pm_col, axis=1, inplace=True)
 
-    print(weatherDataCall)
-    # 3일부터10일까지 강수확률 데이터에서 "regId":"11C20000"인 데이터 추출
-    weather_data_list = [data1 for data1 in treeWeatherData['response']['body']['items']['item'] if
-                         data1.get('regId') == '11C20000']
-
-    print(weather_data_list)
-
-    # 데이터프레임 생성
-    weather_df = pd.DataFrame(weather_data_list, index=None)
-
-    selected_columns3 = ['rnSt3Am', 'rnSt3Pm', 'rnSt4Am', 'rnSt4Pm', 'rnSt5Am', 'rnSt5Pm', 'rnSt6Am', 'rnSt6Pm',
-                         'rnSt7Am', 'rnSt7Pm', 'rnSt8', 'rnSt9', 'rnSt10']
-
-
-    # 인덱스 삭제
-    weather_df_selected3 = weather_df[selected_columns3].reset_index(drop=True)
-
-    weather_df_selected3 = pd.DataFrame(weather_df_selected3)
-
-    # '강수량' 컬럼 추가
-    weather_df_selected3['강수량'] = 0
-
-
-    # 결과 출력
-    print(weather_df_selected3)
-
-
-    # 결과 출력
-    print(weather_df_selected3)
+    # for i in range(8, 11):
+    #     new_col = f'rnSt{i}'
+    #     weather_df_selected3[new_col] = weather_df_selected3.where((weather_df_selected3[new_col] >= 50), 1, 0)
+    # weather_df_selected3 = weather_df_selected3[['rnSt3', 'rnSt4', 'rnSt5', 'rnSt6', 'rnSt7', 'rnSt8','rnSt9', 'rnSt10']].T
+    # weather_df_selected3['강수유무'] = weather_df_selected3
+    # weather_df_selected3.drop(0, axis=1, inplace=True)
+    # print(weather_df_selected3)
     # # '강수량' 컬럼 생성
     # rnst_cols_am_pm = [f'rnSt{i}Am' for i in range(3, 11)] + [f'rnSt{i}' for i in range(8, 11)]
     # weather_df_selected3['강수량'] = weather_df_selected3[rnst_cols_am_pm].apply(tuple, axis=1)
